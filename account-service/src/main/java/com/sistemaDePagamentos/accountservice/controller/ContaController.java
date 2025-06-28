f497db41ea86004e5e4619e95c9fa18056222469
Guilherme Varella <guilhermeraul1@hotmail.com>
2025-06-27 21:30:42 -0300

    feat: endpoints de listagem e busca de conta por ID

--- account-service/src/main/java/com/sistemaDePagamentos/accountservice/controller/ContaController.java
+++ account-service/src/main/java/com/sistemaDePagamentos/accountservice/controller/ContaController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,5 +22,18 @@ public class ContaController {
     public ResponseEntity<Conta> criar (@Valid @RequestBody ContaRequest request){
         Conta novaConta = service.criarConta(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaConta);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Conta>> listar (){
+        List<Conta> contas =  service.listarTodas();
+        return ResponseEntity.ok(contas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Conta> buscarPorId(@PathVariable Long id){
+        Conta conta = service.buscarPorId(id);
+        return ResponseEntity.ok(conta);
     }
 }
--- account-service/src/main/java/com/sistemaDePagamentos/accountservice/service/ContaService.java
+++ account-service/src/main/java/com/sistemaDePagamentos/accountservice/service/ContaService.java
@@ -6,18 +6,23 @@ import com.sistemaDePagamentos.accountservice.model.Cliente;
 import com.sistemaDePagamentos.accountservice.model.Conta;
 import com.sistemaDePagamentos.accountservice.repository.ClienteRepository;
 import com.sistemaDePagamentos.accountservice.repository.ContaRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
-@RequiredArgsConstructor
+
 @Service
 public class ContaService {
 
     private final ContaRepository contaRepository;
     private final ClienteRepository clienteRepository;
+
+    public ContaService(ContaRepository contaRepository, ClienteRepository clienteRepository) {
+        this.contaRepository = contaRepository;
+        this.clienteRepository = clienteRepository;
+    }
 
     public Conta criarConta(ContaRequest request){
         Cliente cliente = clienteRepository.findById(request.getClienteId())
@@ -37,6 +42,15 @@ public class ContaService {
 
     private String gerarNumeroConta(){
         return UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+    }
+
+    public List<Conta> listarTodas(){
+        return contaRepository.findAll();
+    }
+
+    public Conta buscarPorId(Long id){
+        return contaRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Conta não encontrada com o ID: " + id));
     }
 
 }
